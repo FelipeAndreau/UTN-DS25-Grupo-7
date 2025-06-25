2d634127e50c9f0441ed0c446c6bb66ec18cdfc9
ezzz3 <73205104+ezzz37@users.noreply.github.com>
2025-06-25 14:55:29 -0300

    modos de root y proteccion main.jsx

--- Maquetacion/app/src/main.jsx
+++ Maquetacion/app/src/main.jsx
@@ -8,3 +8,17 @@ createRoot(document.getElementById('root')).render(
     <App />
   </StrictMode>,
 )
+
+{/*
+
+ root.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </AuthProvider>
+  </React.StrictMode>
+
+);
+*/}
