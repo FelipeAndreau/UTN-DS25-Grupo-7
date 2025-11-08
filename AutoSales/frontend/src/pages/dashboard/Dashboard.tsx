87573b2a228f7cc705ce21c53c4da715419dc201
Ulises Mateo Bucchino <ulibucchino@gmail.com>
2025-11-08 16:45:32 -0300

    estilos

--- AutoSales/frontend/src/pages/dashboard/Dashboard.tsx
+++ AutoSales/frontend/src/pages/dashboard/Dashboard.tsx
@@ -136,7 +136,7 @@ const Dashboard = () => {
       </div>
 
       {/* Main Content */}
-      <div className="w-4/5 h-full overflow-y-auto p-5">
+      <div className="w-4/5 h-full overflow-y-auto p-5 bg-blue-400">
         {seccionActiva === "dashboard" && (
           <div className="p-6 bg-gray-100 min-h-screen">
             <h1 className="text-2xl font-bold mb-6 text-gray-800 flex items-center justify-between">
